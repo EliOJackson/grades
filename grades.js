17d846fea397c03689b2bdd2d53ed6fe4d5c33a8
Eli Jackson <EliJackson@Elis-MacBook-Pro.local>
2017-12-01 14:34:47 -0600

    able to console log the number of grades

---
+++ grades.js
@@ -0,0 +1,33 @@
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+var aGrade = 0;
+var bGrade = 0;
+var cGrade = 0;
+var dGrade = 0;
+var fGrade = 0;
+
+// function scoreGrades (scores) {
+    for (var i = 0; i < scores.length; i++) {
+        if (scores[i] < 60){
+            fGrade ++;
+        } else if (scores[i] < 70){
+            dGrade ++;
+        } else if (scores[i] < 80){
+            cGrade ++;
+        } else if (scores[i] < 90){
+            bGrade ++;
+        } else {
+            aGrade ++;
+        }
+    };
+    // return (scores);
+
+     
+// };
+console.log("There are " + fGrade + " F grades");
+console.log("There are " + dGrade + " D grades");
+console.log("There are " + cGrade + " C grades");
+console.log("There are " + bGrade + " B grades");
+console.log("There are " + aGrade + " A grades");
+
+
